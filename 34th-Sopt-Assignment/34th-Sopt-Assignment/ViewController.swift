7f003474622e1c466bca5f432c247f5ce8deb1f8
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-04-15 14:38:45 +0900

    [Setting] xcode 파일 추가, .gitignore 파일 추가

---
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  34th-Sopt-Assignment
+//
+//  Created by 정민지 on 4/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
